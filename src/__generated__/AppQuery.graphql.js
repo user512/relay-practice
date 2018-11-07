191940ad9de359b895ceeda28a2c61cb07f3ba32
Tom Lee <user512@users.noreply.github.com>
2018-11-06 21:07:38 -0800

    init setup

---
+++ src/__generated__/AppQuery.graphql.js
@@ -0,0 +1,79 @@
+/**
+ * @flow
+ * @relayHash d0bac3837849e236545f741e1ffae905
+ */
+
+/* eslint-disable */
+
+'use strict';
+
+/*::
+import type { ConcreteRequest } from 'relay-runtime';
+export type AppQueryVariables = {||};
+export type AppQueryResponse = {|
+  +viewer: ?{|
+    +id: string
+  |}
+|};
+export type AppQuery = {|
+  variables: AppQueryVariables,
+  response: AppQueryResponse,
+|};
+*/
+
+
+/*
+query AppQuery {
+  viewer {
+    id
+  }
+}
+*/
+
+const node/*: ConcreteRequest*/ = (function(){
+var v0 = [
+  {
+    "kind": "LinkedField",
+    "alias": null,
+    "name": "viewer",
+    "storageKey": null,
+    "args": null,
+    "concreteType": "User",
+    "plural": false,
+    "selections": [
+      {
+        "kind": "ScalarField",
+        "alias": null,
+        "name": "id",
+        "args": null,
+        "storageKey": null
+      }
+    ]
+  }
+];
+return {
+  "kind": "Request",
+  "operationKind": "query",
+  "name": "AppQuery",
+  "id": null,
+  "text": "query AppQuery {\n  viewer {\n    id\n  }\n}\n",
+  "metadata": {},
+  "fragment": {
+    "kind": "Fragment",
+    "name": "AppQuery",
+    "type": "Query",
+    "metadata": null,
+    "argumentDefinitions": [],
+    "selections": v0
+  },
+  "operation": {
+    "kind": "Operation",
+    "name": "AppQuery",
+    "argumentDefinitions": [],
+    "selections": v0
+  }
+};
+})();
+// prettier-ignore
+(node/*: any*/).hash = '1c19db752baf3ec97c7b454f2dd10241';
+module.exports = node;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,37 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {
+  Environment,
+  Network,
+  RecordSource,
+  Store,
+} from 'relay-runtime';
+
+function fetchQuery(
+  operation,
+  variables,
+) {
+  return fetch('http://localhost:4000/graphql', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      query: operation.text,
+      variables,
+    }),
+  }).then(response => {
+    return response.json();
+  });
+}
+
+const environment = new Environment({
+  network: Network.create(fetchQuery),
+  store: new Store(new RecordSource()),
+});
+
+export default environment;
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
